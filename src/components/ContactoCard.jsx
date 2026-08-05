a8b8910833f194b6ba219bfb65f39b47307937c9
Juan De Los Rios <delosriosjuan14@gmail.com>
2026-08-04 20:24:34 -0500

    Clase_5_Agenda_ADSO_v3_LocalStorage

--- src/components/ContactoCard.jsx
+++ src/components/ContactoCard.jsx
@@ -1,49 +1,12 @@
-// // ContactoCard.jsx
-// // Este componente muestra UN contacto de la agenda.
-// // Recibe datos (props): nombre, telefono, correo, etiqueta.
-
-// export default function ContactoCard({ nombre, telefono, correo, etiqueta }) {
-//   return (
-//     <div className="card-contacto">
-//       {/* Nombre del contacto en negrita */}
-//       <h3 className="card-nombre">{nombre}</h3>
-
-//       {/* Teléfono */}
-//       <p className="card-linea">📞 {telefono}</p>
-
-//       {/* Correo */}
-//       <p className="card-linea">📧 {correo}</p>
-
-//       {/* Etiqueta adicional, si existe (por ejemplo: "Instructor", "Cliente", "Compañera") */}
-//       {etiqueta && (
-//         <p className="card-etiqueta">{etiqueta}</p>
-//       )}
-//     </div>
-//   );
-// }
-
-export default function ContactoCard({
-  id,
-  nombre,
-  telefono,
-  correo,
-  etiqueta,
-  onDelete,
-}) {
+export default function ContactoCard({ nombre, telefono, correo, etiqueta, onEliminar }) {
   return (
     <article className="tarjeta-contacto">
-      <h3>{nombre} </h3>
-      {etiqueta && <p className="tag">{etiqueta}</p>}
+      <h3>{nombre}</h3>
       <p>📞 {telefono}</p>
-      {correo && <p>✉️ {correo}</p>}
-      
-
+      <p>✉️ {correo}</p>
+      {etiqueta && <p>{etiqueta}</p>}
       <div className="acciones">
-        <button
-          type="button"
-          className="btn-eliminar"
-          onClick={() => onDelete(id)}
-        >
+        <button className="btn-eliminar" onClick={() => onEliminar(correo)}>
           Eliminar
         </button>
       </div>
